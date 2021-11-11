c6171b381d9e9d30c0f829d045117efb7edc78c1
David Pereira <50082994+davidpereiracib@users.noreply.github.com>
2021-11-11 16:08:08 -0500

    Update Cambiar_ la_MAC.py

--- Cambiar_ la_MAC.py
+++ Cambiar_ la_MAC.py
@@ -1,14 +1,19 @@
 #Debes colocar como nombre de este archivo: main.py, por que de lo contrario no funcionaría; en el próximo video explico en más detalle.
 
+import subprocess
 
 if __name__ == '__main__':
     interfase="eth0"
     nueva_MAC="22:11:33:44:55:14"
+    
     print("Desactivando la interfase")
     subprocess.run(["ifconfig","eth0","down"])
+    
     print("Cambiando la direccion MAC de la interfase:", interfase, " a ", nueva_MAC)
     subprocess.run(["ifconfig",interfase, "hw","ether",nueva_MAC])
+    
     print("La direccion MAC cambio a: ", nueva_MAC)
     subprocess.run(["ifconfig",interfase, "up"])
+    
     print("La Interfase esta lista")
     subprocess.run(["ifconfig"])
